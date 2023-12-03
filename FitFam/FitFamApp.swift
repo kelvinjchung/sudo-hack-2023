ee98bafd581e73d8550e2673910f06e9b8e9ae8b
kelvinjchung <kchung1223@gmail.com>
2023-12-02 19:44:52 -0800

    Initial Commit

---
+++ FitFam/FitFamApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitFamApp.swift
+//  FitFam
+//
+//  Created by Kelvin Chung on 12/2/23.
+//
+
+import SwiftUI
+
+@main
+struct FitFamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
